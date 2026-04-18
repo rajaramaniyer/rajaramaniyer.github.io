0b2bae1625563f1574e3f457520747e3138ed2da
Rajaraman Iyer <rajaramaniyer@gmail.com>
2026-04-18 13:15:03 +0800

    update service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,6 @@
-const staticDevCoffee = "web-harmonium";
+// 1. Versioning: Change this string every time you update your app!
+const staticDevCoffee = "web-harmonium-v2"; 
+
 const assets = [
   "/",
   "/webharmonium.html",
@@ -6,9 +8,24 @@ const assets = [
 ];
 
 self.addEventListener("install", installEvent => {
+  // 2. Immediate Takeover: Forces this new worker to become active right away
+  self.skipWaiting();
+  
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Clean up old caches when the new version activates
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== staticDevCoffee)
+            .map(key => caches.delete(key))
+      );
     })
   );
 });
@@ -16,6 +33,9 @@ self.addEventListener("install", installEvent => {
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
+      // 3. Network First (or Stale-While-Revalidate)
+      // This logic says: If we have it in cache, return it, 
+      // but also go get the fresh version from the network.
       return res || fetch(fetchEvent.request);
     })
   );
